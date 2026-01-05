0bf78ec08191b3e6dd2d74eaa37bc17f7c9875a8
Amychow <13067802137@163.com>
2026-01-05 23:07:40 +0800

    ✨ feat: 新增redis数据库和连接使用方法

---
+++ backend/app/crud/redis.py
@@ -0,0 +1,41 @@
+from fastapi import HTTPException
+from fastapi import FastAPI, Depends
+import aioredis
+from db.redis_session import get_db
+
+# 创建/设置数据
+async def set_key(key: str, value: str, redis: aioredis.Redis = Depends(get_db)):
+    try:
+        await redis.set(key, value)  # 设置键值对
+        return {"message": "Key set successfully"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# 获取数据
+async def get_key(key: str, redis: aioredis.Redis = Depends(get_db)):
+    value = await redis.get(key)  # 获取键对应的值
+    if value is None:
+        raise HTTPException(status_code=404, detail="Key not found")
+    return {"key": key, "value": value}
+
+
+# 更新数据
+async def update_key(key: str, value: str, redis: aioredis.Redis = Depends(get_db)):
+    exists = await redis.exists(key)
+    if not exists:
+        raise HTTPException(status_code=404, detail="Key not found")
+    await redis.set(key, value)  # 更新键值对
+    return {"message": "Key updated successfully"}
+
+# 删除数据
+async def delete_key(key: str, redis: aioredis.Redis = Depends(get_db)):
+    result = await redis.delete(key)  # 删除指定键
+    if result == 0:
+        raise HTTPException(status_code=404, detail="Key not found")
+    return {"message": "Key deleted successfully"}
+
+"""
+redis.set(key, value, ex=expiration)  # 设置过期时间
+还有管道功能和事务管理
+"""
+
--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,18 @@ from fastapi import FastAPI
 from app.api import router
 from app.core.config import settings
 import fastapi_cdn_host
+from db.redis_session import get_redis_connection
 
 
 app = FastAPI(title=settings.PROJECT_NAME)
 fastapi_cdn_host.patch_docs(app)
 
+# 在应用启动时初始化 Redis 连接
+@app.on_event("startup")
+async def on_startup():
+    redis = await get_redis_connection()
+    print("Redis connection initialized")
+    await redis.close()
 
 # 注册API路由
 app.include_router(router.api_router, prefix="/api")
